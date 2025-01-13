eb11b3a736703bf1fc1ab185d70a4c88ef34040c
허예찬 <baoro0812@gmail.com>
2025-01-13 21:31:24 +0900

    템플릿 함(404전)

--- polls/views.py
+++ polls/views.py
@@ -4,14 +4,27 @@ from django.template import loader
 from .models import Question
 
 # Create your views here.
+
+# 1 string 반환
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     output = ", ".join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+
+# 2 템플릿 활용
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     template = loader.get_template("polls/index.html")
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# 3 render() 활용
 def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    # output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(template.render(context, request))
+    latest_question_list = Question.objects.order_by("pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
